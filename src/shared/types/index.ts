2887965af418f4540fd9221c86c2db2fe7a83992
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-29 20:57:44 +0300

    types

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,1 +1,25 @@
-export * from './interfaces'
+export interface Post {
+  userId: number
+  id: number
+  title: string
+  body: string
+  // sort?: string
+}
+
+export interface Comment {
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
+}
+
+export interface FilterField {
+  sort: string
+  query: string
+}
+
+export interface Options {
+  value: string
+  name: string
+}
