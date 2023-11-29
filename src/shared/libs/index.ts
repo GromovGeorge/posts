72ec121cbcea7ecaf1637a4019eca50e4a9082ad
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-29 20:57:10 +0300

    add functions

--- src/shared/libs/index.ts
+++ src/shared/libs/index.ts
@@ -1,2 +1,11 @@
-export * from './getPageCount'
-export * from './getPagesArray'
+export const getPageCount = (totalCount: number, limit: number) => {
+  return Math.ceil(totalCount / limit)
+}
+
+export const getPagesArray = (totalPages: number) => {
+  let result = []
+  for (let i = 0; i < totalPages; i++) {
+    result.push(i + 1)
+  }
+  return result
+}
