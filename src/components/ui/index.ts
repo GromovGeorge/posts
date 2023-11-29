c26a454e01fe99bdbe4b15497dad10038480ffcd
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-29 20:57:17 +0300

    exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './button/Button'
+export { default as Input } from './input/Input'
+export { default as Loader } from './loader/Loader'
+export { default as Modal } from './modal/Modal'
+export { default as Pagination } from './pagination/Pagination'
+export { default as Select } from './select/Select'
