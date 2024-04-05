b0ea70a3022ade1b08efb610f047eaf9b8c854d2
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-04-06 01:46:59 +0300

    refactor: use const instead class

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,26 +2,26 @@ import axios from 'axios'
 import { API } from '../shared/constants'
 import { Comment, Post } from '../shared/types'
 
-export class ApiService {
-  static async getUsers() {
+export const ApiService = {
+  async getUsers() {
     return await axios.get<Post>(`${API}/users`)
-  }
+  },
 
-  static async getUserById(id: number) {
+  async getUserById(id: number) {
     return await axios.get<Post>(`${API}/users/${id}`)
-  }
+  },
 
-  static async getPosts(limit = 10, page = 1) {
+  async getPosts(limit = 10, page = 1) {
     return await axios.get<Post[]>(`${API}/posts`, {
       params: { _limit: limit, _page: page },
     })
-  }
+  },
 
-  static async getPostById(id: number) {
+  async getPostById(id: number) {
     return await axios.get<Post>(`${API}/posts/${id}`)
-  }
+  },
 
-  static async getComments(postId: number) {
+  async getComments(postId: number) {
     return await axios.get<Comment[]>(`${API}/comments?postId=${postId}`)
-  }
+  },
 }
