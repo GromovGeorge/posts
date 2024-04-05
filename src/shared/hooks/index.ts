54875c5215d0ebb0844dbc8c939b54c3bda5abeb
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-04-06 01:47:35 +0300

    feat: impl useObserver hook

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -75,3 +75,20 @@ export const useSearchedPosts = (posts: Post[], sort: string, query: string) => 
 }
 
 export const usePagination = () => {}
+
+export const useObserver = (ref, canLoad, isLoading, callback) => {
+  const observer = useRef()
+
+  useEffect(() => {
+    if (isLoading) return
+    if (observer.current) observer.current.disconnect()
+
+    var cb = function (entries, observer) {
+      if (entries[0].isIntersecting && canLoad) {
+        callback()
+      }
+    }
+    observer.current = new IntersectionObserver(cb)
+    observer.current.observe(ref.current)
+  }, [isLoading])
+}
